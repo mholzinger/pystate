1e81efc88bc46f89a816bfe535ec11c9ededf431
Mike Holzinger <mholzinger@us.ibm.com>
2017-06-30 16:15:45 -0700

    rsync all save files on local pi to ~/saves

--- copy_roms.sh
+++ copy_roms.sh
@@ -1,5 +1,9 @@
 copy_to_path=~/saves
 rom_path=~/RetroPie/roms
 
-find "$rom_path"  \( -iname "*.state" -o -iname "*.sav*" -o -iname "*srm" \) -printf '%h\n' -exec mkdir -p -- "$copy_to_path"/{} \;
+# This copies only the folder path
+# find "$rom_path"  \( -iname "*.state" -o -iname "*.sav*" -o -iname "*srm" \) -printf '%h\n' -exec mkdir -p -- "$copy_to_path"/{} \;
 
+# This does an rsync with all the right file extensions
+# rsync -a --include '*/' --include '*.mp3' --exclude '*' source/ target/
+rsync -a --include '*/' --include '*.srm' --include '*.sav*' --include '*.state' --exclude '*' "$rom_path" "$copy_to_path"
