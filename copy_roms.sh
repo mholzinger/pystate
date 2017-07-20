b17be4eee0dd9d2a4870927eec8031fa1ce9242a
Mike Holzinger <mike.holzinger@gmail.com>
2017-07-20 15:00:35 -0700

    copy rom config entries

--- copy_roms.sh
+++ copy_roms.sh
@@ -1,9 +1,16 @@
-copy_to_path=~/saves
+save_copy_path=~/saves
+config_copy_path=~/configs
+
 rom_path=~/RetroPie/roms
+config_path=/opt/retropie/configs/all/retroarch/config/
 
 # This copies only the folder path
 # find "$rom_path"  \( -iname "*.state" -o -iname "*.sav*" -o -iname "*srm" \) -printf '%h\n' -exec mkdir -p -- "$copy_to_path"/{} \;
 
 # This does an rsync with all the right file extensions
 # rsync -a --include '*/' --include '*.mp3' --exclude '*' source/ target/
-rsync -a --include '*/' --include '*.srm' --include '*.sav*' --include '*.state' --exclude '*' "$rom_path" "$copy_to_path"
+rsync -a --include '*/' --include '*.srm' --include '*.sav*' --include '*.state' --exclude '*' "$rom_path" "$save_copy_path"
+
+
+# This does an rsync with all the rom specefic retroarch settings
+rsync -a --include '*/' --include '*.cfg' --exclude '*' "$config_path" "$config_copy_path"
